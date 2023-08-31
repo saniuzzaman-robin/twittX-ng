90b2e5af4135b606f9a7ade6e4505c863352b0d5
Saniuzzaman Robin <32646654+saniuzzaman-robin@users.noreply.github.com>
2023-08-31 17:10:33 +0600

    app name & dist folder name modification to match all instance

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TwittXNg
+# twitt-x-Ng
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'twittX-ng'`, () => {
+  it(`should have as title 'twitt-x-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('twittX-ng');
+    expect(app.title).toEqual('twitt-x-ng');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'twittX-ng app is running!'
+      'twitt-x-ng app is running!'
     );
   });
 });
